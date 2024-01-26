0f7ea0586813d9cee7b6b3f6fee6e65d8558e4d0
Julirey <reyes.0271204@gmail.com>
2024-01-26 02:48:21 -0400

    Add hamburguer menu

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,12 @@ let lastModified = document.lastModified;
 // Set new values
 ParagraphElement.innerHTML = `©${currentYear}`;
 lastModifiedElement.innerHTML = `Last Modification: ${lastModified}`;
+
+// Hamburguer menu button
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
