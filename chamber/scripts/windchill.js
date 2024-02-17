5769e66435dfc1a663f64332f45630d600417d43
Julirey <reyes.0271204@gmail.com>
2024-02-17 03:02:10 -0400

    Add ability to calculate windchill

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,19 @@
+// Windchill
+
+const temp = document.querySelector("#temperature").innerHTML;
+const wSpeed = document.querySelector("#windspeed").innerHTML;
+const wChillEle = document.querySelector("#windchill");
+
+const windChillCelsius = (temperature, windSpeed) =>
+  13.12 +
+  0.6215 * temperature -
+  11.37 * windSpeed ** 0.16 +
+  0.3965 * temperature * windSpeed ** 0.16;
+
+if (temp <= 50 && wSpeed > 3) {
+    let wChill = windChillCelsius(temp, wSpeed);
+    console.log(wChill);
+    windchill.innerHTML = wChill.toFixed(2);
+} else {
+    windchill.innerHTML = "N/a"; 
+}
