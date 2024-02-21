bce01cbe601651cccdc81958980da07d9e9e9953
Julirey <reyes.0271204@gmail.com>
2024-02-21 17:02:44 -0400

    Add page visit counter

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,16 @@ hamButton.addEventListener("click", () => {
 	navigation.classList.toggle("open");
 	hamButton.classList.toggle("open");
 });
+
+// Number of page visits
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `Welcome! This is your first visit.`;
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
