82e1ae133f178c55c958df2277e2bc3a5f74848b
Julirey <reyes.0271204@gmail.com>
2024-04-12 03:26:28 -0400

    Normalize formatting and add some grids

--- scoots/scripts/main.js
+++ scoots/scripts/main.js
@@ -18,8 +18,8 @@ const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 
 hamButton.addEventListener("click", () => {
-	navigation.classList.toggle("open");
-	hamButton.classList.toggle("open");
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
 });
 
 // Dark mode
@@ -27,81 +27,81 @@ const modeButton = document.querySelector("#check");
 const body = document.querySelector("body");
 
 modeButton.addEventListener("click", () => {
-	if (modeButton.checked) {
-		body.style.background = "#000";
-		body.style.color = "#fff";
-		changeCardColor("#2c2f33");
-		changeCardLinkColor("white");
-		changeTitleColor("orangered")
-		if (page === "reservations.html" || page === "contact.html") {
-			changeFormColor("#2c2f33", "white", "white");
-		};
-	} else {
-		body.style.background = "";
-		body.style.color = "";
-		changeCardColor("");
-		changeCardLinkColor("")
-		changeTitleColor("")
-		if (page === "reservations.html" || page === "contact.html") {
-			changeFormColor("", "", "");
-		};
-	}
+  if (modeButton.checked) {
+    body.style.background = "#000";
+    body.style.color = "#fff";
+    changeCardColor("#2c2f33");
+    changeCardLinkColor("white");
+    changeTitleColor("orangered");
+    if (page === "reservations.html" || page === "contact.html") {
+      changeFormColor("#2c2f33", "white", "white");
+    }
+  } else {
+    body.style.background = "";
+    body.style.color = "";
+    changeCardColor("");
+    changeCardLinkColor("");
+    changeTitleColor("");
+    if (page === "reservations.html" || page === "contact.html") {
+      changeFormColor("", "", "");
+    }
+  }
 });
 
 function changeTitleColor(color) {
-	let titles = document.querySelectorAll("main h3");
-	for (const element of titles) {
-		let title = element;
-		title.style.background = color;
-	}
+  let titles = document.querySelectorAll("main h3");
+  for (const element of titles) {
+    let title = element;
+    title.style.background = color;
+  }
 }
 
 function changeCardColor(color) {
-	let cards = document.querySelectorAll(".card");
-	for (const element of cards) {
-		let card = element;
-		card.style.background = color;
-	}
+  let cards = document.querySelectorAll(".card");
+  for (const element of cards) {
+    let card = element;
+    card.style.background = color;
+  }
 }
 
 function changeCardLinkColor(color) {
-	let cards = document.querySelectorAll(".card");
-	for (const element of cards) {
-		let card = element
-		let links = card.querySelectorAll("a");
-		for (const element of links) {
-			let link = element
-			link.style.color = color;
-		}
-	}
+  let cards = document.querySelectorAll(".card");
+  for (const element of cards) {
+    let card = element;
+    let links = card.querySelectorAll("a");
+    for (const element of links) {
+      let link = element;
+      link.style.color = color;
+    }
+  }
 }
 
 function changeFormColor(color, color2, color3) {
-	let form = document.querySelector("fieldset");
-	form.style.background = color;
+  let form = document.querySelector("fieldset");
+  form.style.background = color;
 
-	let legend = form.querySelector("legend");
-	legend.style.color = color2;
+  let legend = form.querySelector("legend");
+  legend.style.color = color2;
 
-	let divs = form.querySelectorAll("div");
-	for (const element of divs) {
-		let div = element
-		div.style.color = color2;
-	}
+  let divs = form.querySelectorAll("div");
+  for (const element of divs) {
+    let div = element;
+    div.style.color = color2;
+  }
 
-	let labels = form.querySelectorAll("label");
-	for (const element of labels) {
-		let label = element
-		label.style.color = color3;
-	}
-};
+  let labels = form.querySelectorAll("label");
+  for (const element of labels) {
+    let label = element;
+    label.style.color = color3;
+  }
+}
 
-// ToggleBanner  
+// ToggleBanner
 if (page === "index.html" || page === "") {
-	const bannerEle = document.querySelector("#banner");
-	const bannerButton = document.querySelector("#closeBanner")
+  const bannerEle = document.querySelector("#banner");
+  const bannerButton = document.querySelector("#closeBanner");
 
-	bannerButton.addEventListener("click", () => {
-		bannerEle.style.display = "none";
-	});
-};
+  bannerButton.addEventListener("click", () => {
+    bannerEle.style.display = "none";
+  });
+}
--- scoots/scripts/weather.js
+++ scoots/scripts/weather.js
@@ -1,7 +1,7 @@
 // Select HTML elements in the document
 const currentTemp = document.querySelector("#temperature");
 const currentHumid = document.querySelector("#humidity");
-const weatherGroup = document.querySelector("#grouptype")
+const weatherGroup = document.querySelector("#grouptype");
 const weatherDesc = document.querySelector("#weather-desc");
 const weatherIcon = document.querySelector(".weather-icon");
 
@@ -10,7 +10,8 @@ const wChillEle = document.querySelector("#windchill");
 
 const maxTemp = document.querySelector("#maxTemp");
 
-const url = "https:///api.openweathermap.org/data/2.5/weather?lat=20.51&lon=-86.94&units=imperial&appid=3bf678e6bdd1c1720ca9fc7f3fd188a9"
+const url =
+  "https:///api.openweathermap.org/data/2.5/weather?lat=20.51&lon=-86.94&units=imperial&appid=3bf678e6bdd1c1720ca9fc7f3fd188a9";
 
 // Fetch current weather
 async function apiFetch() {
@@ -20,14 +21,14 @@ async function apiFetch() {
       const data = await response.json();
       displayResults(data);
     } else {
-        throw Error(await response.text());
+      throw Error(await response.text());
     }
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 }
 
-// Display 
+// Display
 function displayResults(data) {
   let iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
   let group = data.weather[0].main;
@@ -36,7 +37,7 @@ function displayResults(data) {
   let humid = data.main.humidity;
   let wSpeed = data.wind.speed;
   let tempMax = data.main.temp_max;
-  
+
   weatherIcon.setAttribute("src", iconsrc);
   weatherIcon.setAttribute("alt", toTitleCase(desc));
   weatherDesc.innerHTML = `<strong>${toTitleCase(desc)}</strong>`;
@@ -50,11 +51,11 @@ function displayResults(data) {
     let wChill = windChillCelsius(temp, wSpeed);
     wChillEle.innerHTML = wChill.toFixed(2);
   } else {
-    wChillEle.innerHTML = "N/a"; 
+    wChillEle.innerHTML = "N/a";
   }
 }
 
-// Select elements for forecast 
+// Select elements for forecast
 const forecastElements = Array.from(
   document.getElementsByClassName("tempforecast")
 );
@@ -62,9 +63,10 @@ const forecastImgElements = Array.from(
   document.getElementsByClassName("weather-icon")
 );
 
-const urlForecast = "https:///api.openweathermap.org/data/2.5/forecast?lat=20.51&lon=-86.94&units=imperial&appid=3bf678e6bdd1c1720ca9fc7f3fd188a9"
+const urlForecast =
+  "https:///api.openweathermap.org/data/2.5/forecast?lat=20.51&lon=-86.94&units=imperial&appid=3bf678e6bdd1c1720ca9fc7f3fd188a9";
 
-// Fetch forecast 
+// Fetch forecast
 async function apiFetchForecast() {
   try {
     const response = await fetch(urlForecast);
@@ -75,7 +77,7 @@ async function apiFetchForecast() {
       throw Error(await response.text());
     }
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 }
 
@@ -87,24 +89,26 @@ function displayForecast(data) {
     let temp = data.list[i].main.temp;
 
     forecastElements[i].innerHTML = `Temperature: ${temp}&deg;F`;
-    forecastImgElements[i+1].setAttribute("src", iconsrc);
-    forecastImgElements[i+1].setAttribute("alt", toTitleCase(desc));
-  } 
+    forecastImgElements[i + 1].setAttribute("src", iconsrc);
+    forecastImgElements[i + 1].setAttribute("alt", toTitleCase(desc));
+  }
 }
 
 // Windchill
 function windChillCelsius(temperature, windSpeed) {
-  return 13.12 + 0.6215 * temperature - 11.37 * windSpeed ** 0.16 + 0.3965 * temperature * windSpeed ** 0.16;
+  return (
+    13.12 +
+    0.6215 * temperature -
+    11.37 * windSpeed ** 0.16 +
+    0.3965 * temperature * windSpeed ** 0.16
+  );
 }
 
-// Titlecase converter 
+// Titlecase converter
 function toTitleCase(str) {
-  return str.replace(
-    /\w\S*/g,
-    function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }
-  );
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
 
 apiFetch();
